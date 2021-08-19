fe040af78b6e4fb8077ff55e75c455a9d7767430
tordnat <tordna@protonmail.com>
2021-08-19 16:17:24 +0200

    fix: add automatic ENV vars for rootless docker

--- scripts/dockerInstall.sh
+++ scripts/dockerInstall.sh
@@ -21,8 +21,14 @@ sudo docker run hello-world
 echo  "${GREEN} Setting up rootless docker ${BLANK}"
 sudo apt-get install uidmap
 dockerd-rootless-setuptool.sh install
-: ${PATH:=/usr/bin:$PATH}
-: ${DOCKER_HOST:=unix:///run/user/1000/docker.sock}
+
+#Add to path
+BINARY_PATH="PATH:=/usr/bin:$PATH"
+if ! grep -qF "$BINARY_PATH" ~/.bashrc; then echo "$BINARY_PATH" >> ~/.bashrc ; source ~/.bashrc ; fi
+SET_DOCKER_HOST="DOCKER_HOST:=unix:///run/user/1000/docker.sock"
+if ! grep -qF "$SET_DOCKER_HOST" ~/.bashrc; then echo "$SET_DOCKER_HOST" >> ~/.bashrc ; source ~/.bashrc ; fi
+
+echo  "${GREEN} Testing rootless docker ${BLANK}"
 docker run docker/whalesay cowsay Docker Installed Successfully
 sudo systemctl daemon-reload
 sudo systemctl restart docker
