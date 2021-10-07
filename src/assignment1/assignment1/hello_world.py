1a992b4e59928160b5a9bf32858bad8c0f268b07
eiriksteen <72987142+eiriksteen@users.noreply.github.com>
2021-10-07 20:23:42 +0200

    Update hello_world.py

--- src/assignment1/assignment1/hello_world.py
+++ src/assignment1/assignment1/hello_world.py
@@ -14,7 +14,7 @@ class HelloNode(Node):
         self.i = 0
 
     def timer_callback(self):
-        msg = String() #Create message type string
+        msg = String( #Create message type string
         msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg) # Publish message
         self.get_logger().info('Publishing: "%s"' % msg.data) # Log to console
